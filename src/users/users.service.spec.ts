495f483a67566e513940d5e81a3e7f016d57ec29
soyoung kim <soyou6358@gmail.com>
2020-12-15 11:18:58 +0900

    #7.2 add mock for test

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,71 @@
+import { Test } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/users.entity';
+import { Verification } from './entities/verification.entity';
+import { MailService } from 'src/mail/mail.service';
+import { JwtService } from 'src/jwt/jwt.service';
+import { Repository } from 'typeorm';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerfication: jest.fn(),
+};
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+describe('UserService', () => {
+  let service: UsersService;
+  let usersRepository: MockRepository<User>;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {});
+  });
+
+  it.todo(`createAccount`, () => {});
+  it.todo(`login`, () => {});
+  it.todo(`findById`, () => {});
+  it.todo(`editProfile`, () => {});
+  it.todo(`verifyEmail`, () => {});
+});
